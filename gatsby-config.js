523326679573bf62efa052fe5dd1e99d44f39d70
tsamantanis <philip.tsamantanis@icloud.com>
2023-11-23 14:02:15 +0200

    Add serveless function and vercel.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,29 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    {
+      /**
+       * First up is the WordPress source plugin that connects Gatsby
+       * to your WordPress site.
+       *
+       * visit the plugin docs to learn more
+       * https://github.com/gatsbyjs/gatsby/blob/master/packages/gatsby-source-wordpress/README.md
+       *
+       */
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        // the only required plugin option for WordPress is the GraphQL url.
+        url:
+          process.env.WPGRAPHQL_URL ||
+          `https://wpgatsbydemo.wpengine.com/graphql`,
+      },
+      schema: {
+        perPage: 10, // currently set to 100
+        requestConcurrency: 15, // currently set to 15
+        previewRequestConcurrency: 5, // currently set to 5
+        timeout: 990000, // currently set to 100000
+      }
+    },
+  ],
 }
